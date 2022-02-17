270ed3943ba75e431d50d8d5498d73fbbc3691e5
Emil Arzumanov <22-emil@mail.ru>
2022-02-17 15:52:31 +0500

    Added Products from API

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React,{useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {setProducts} from "../redux/actions/productActions";
+import ProductComponent from "./ProductComponent";
+import axios from "axios";
 
 const ProductListing = () => {
+    const products = useSelector((state) => state);
+    const dispatch = useDispatch()
+
+    const fetchProducts = async () => {
+        const response = await axios
+            .get("https://fakestoreapi.com/products")
+            .catch((err) => {
+                console.log("Err",err);
+            });
+        dispatch(setProducts(response.data ));
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    },[]);
+    console.log("Products: ", products);
     return (
-        <div>
-            <h1>ProductListing</h1>
+        <div className="ui grid container">
+            <ProductComponent/>
         </div>
     );
 };
