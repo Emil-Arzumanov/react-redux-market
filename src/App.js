cb5fde365415f40bf2299b503f3f13d0728df372
Emil Arzumanov <22-emil@mail.ru>
2022-02-17 14:31:24 +0500

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from "./containers/Header";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Header/>
+                <Routes>
+                    <Route path="/" exact element={<ProductListing/>}/>
+                    <Route path="/product/:productId" exact element={<ProductDetail/>}/>
+                    <Route>404 Not Found</Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
