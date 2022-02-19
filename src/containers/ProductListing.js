12e6d4c5ec60f00bb32df273658ffaca498b1b5a
Emil Arzumanov <22-emil@mail.ru>
2022-02-19 17:46:36 +0500

    Added Products from API

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,11 +1,10 @@
 import React,{useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {setProducts} from "../redux/actions/productActions";
 import ProductComponent from "./ProductComponent";
 import axios from "axios";
 
 const ProductListing = () => {
-    const products = useSelector((state) => state);
     const dispatch = useDispatch()
 
     const fetchProducts = async () => {
@@ -19,8 +18,8 @@ const ProductListing = () => {
 
     useEffect(() => {
         fetchProducts();
-    },[]);
-    console.log("Products: ", products);
+    });
+
     return (
         <div className="ui grid container">
             <ProductComponent/>
